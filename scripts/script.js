526fb343f1c6887ff7727ee5c1b082b94d8b8084
Zhukov Alexey <zhukov.alexey2017@gmail.com>
2020-08-08 02:13:35 +0300

    Начать писать js

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+let Profile = document.querySelector(".profile");
+let ProfileName = profile.querySelector(".profile__title");
+let ProfileProfession = profile.querySelector(".profile__subtitle");
+let ProfileEditButton = document.querySelector(".button_type_edit");
+
+let Popup = document.querySelector(".popup");
+let PopupInputName = Popup.querySelector("#input-name");
+let PopupInputProfession = Popup.querySelector("#input-profession");
+let PopupSaveButton = document.querySelector(".button_type_save");
+let PopupCloseButton = document.querySelector(".button_type_close");
