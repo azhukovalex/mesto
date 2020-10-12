1d84e861fdf3fbe6cc71002f99994b297eb8d954
unknown <zhukov.alexey2017@gmail.com>
2020-10-12 20:27:49 +0300

    сделать исправления после 1  проверки

--- src/Components/PopupWithForm.js
+++ src/Components/PopupWithForm.js
@@ -12,21 +12,14 @@ export default class PopupWithForm extends Popup {
     this._popupElement.querySelector(".popup__form").reset();
   }
 
-  _getInputValues() {
-   
+  _getInputValues() {   
     this._inputList = this._popupElement.querySelectorAll(
       ".popup__form-input"
-    );
-
-    
-    this._formValues = {};
-
-    
+    );    
+    this._formValues = {};    
     this._inputList.forEach((input) => {
       this._formValues[input.name] = input.value;
-    });
-
-    
+    });    
     return this._formValues;
   }
 
@@ -38,29 +31,6 @@ export default class PopupWithForm extends Popup {
         evt.preventDefault();
         this._submit(this._getInputValues());
         this.close();
-      });
-  }
-
-  cleanError() { //Удалить здесь, и сделать похожий функц. в FormValidator
-    const buttonSave = this._popupElement.querySelector(".button_type_save");
-    this._popupElement
-      .querySelectorAll(".popup__span-error")
-      .forEach((span) => {
-        span.classList.remove("popup__span-error_active"); 
-        span.textContent = "";
-      });
-    this._popupElement
-      .querySelectorAll(".popup__form-input")
-      .forEach((input) => {
-        if (!input.value) {
-          
-          buttonSave.classList.add("button_type_save_inactive"); 
-          buttonSave.setAttribute("disabled", "true");
-        } else {
-          buttonSave.classList.remove("button_type_save_inactive"); 
-          buttonSave.removeAttribute("disabled");
-        }
-        input.classList.remove("popup__form-input_type_error"); 
       });
   }
 }
